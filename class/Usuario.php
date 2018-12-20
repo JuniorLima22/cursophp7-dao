7f7b08270f847bb3c12d6b2dce5c6cd758eb8c09
JuniorLima22 <negstonseventos@hotmail.com>
2018-12-20 18:09:25 -0300

    Adcionado novos métodos na Classe Usuario

--- class/Usuario.php
+++ class/Usuario.php
@@ -37,6 +37,7 @@
 			$this->dtcadastro = $value;
 		}
 
+		//Lista Usuário correspodente ao seu ID especificado
 		public function loadById($id){
 			$sql = new Sql();
 
@@ -56,6 +57,47 @@
 				$this->setDtcadastro(new DateTime($row['dtcadastro']));
 			}
 		}//End: loadById
+
+		//Listar todos os Usuários da Tabela
+		public static function getList(){
+			$sql = new Sql();
+
+			return $sql->select("SELECT * FROM tb_usuarios_dois ORDER BY idusuario;");
+		}//End: getList
+
+		//Faz uma busca de Usuario
+		public static function search($login){
+			$sql = new Sql();
+
+			return $sql->select("SELECT * FROM tb_usuarios_dois WHERE deslogin LIKE :SEARCH ORDER BY idusuario", array(
+				':SEARCH'=>"%".$login."%"
+			));
+		}//End: search
+
+		//Obter dados do usuário Autenticado, tem que passar os dois parametros Login e Senha
+		public function login($login, $password){
+			$sql = new Sql();
+
+			$results = $sql->select("SELECT * FROM tb_usuarios_dois WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+				":LOGIN"=>$login,
+				":PASSWORD"=>$password
+			));
+
+			//Validar os registros: count ou isset
+			// if (isset($results[0])){};
+
+			if (count($results) > 0){
+				$row = $results[0];
+
+				$this->setIdusuario($row['idusuario']);
+				$this->setDeslogin($row['deslogin']);
+				$this->setDessenha($row['dessenha']);
+				$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			}else{
+				throw new Exception("Login e/ou senha inválidos.");
+			}
+
+		}//End: login
 
 		public function __toString(){
 			return json_encode(array(
